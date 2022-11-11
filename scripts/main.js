efd1f8c088729c3d7cb76427bffbf323a4cd8d92
Jerwin <jerwin.jek@gmail.com>
2022-11-11 10:44:26 +0800

    responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 window.addEventListener('load', () => {
 
     const menuBtn = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileNavCont = document.querySelector('.mobile-nav-container');
+    const header = document.querySelector('header');
 
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('is-active');
+        mobileNavCont.classList.toggle('is-active');
     });
 
-
+    mobileNav.style.paddingTop = header.offsetHeight + "px";
 })
